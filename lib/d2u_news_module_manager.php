da675ba501b60c94dd00c97cc364d10e328db9ad
Tobias Krais <tk@design-to-use.de>
2018-05-03 01:32:02 +0200

    1.1.0

--- lib/d2u_news_module_manager.php
+++ lib/d2u_news_module_manager.php
@@ -13,13 +13,13 @@ class D2UNewsModules {
 		$modules = [];
 		$modules[] = new D2UModule("40-1",
 			"D2U News - Ausgabe News",
-			2);
+			3);
 		$modules[] = new D2UModule("40-2",
 			"D2U News - Ausgabe Messen",
 			1);
 		$modules[] = new D2UModule("40-3",
 			"D2U News - Ausgabe News und Messen",
-			1);
+			2);
 		return $modules;
 	}
 }
--- update.php
+++ update.php
@@ -7,13 +7,13 @@ if(class_exists(D2UModuleManager)) {
 	$modules = [];
 	$modules[] = new D2UModule("40-1",
 		"D2U News - Ausgabe News",
-		1);
+		3);
 	$modules[] = new D2UModule("40-2",
 		"D2U News - Ausgabe Messen",
 		1);
 	$modules[] = new D2UModule("40-3",
 		"D2U News - Ausgabe News und Messen",
-		1);
+		2);
 	$d2u_module_manager = new D2UModuleManager($modules, "", "d2u_news");
 	$d2u_module_manager->autoupdate();
 }
