7f673b124b72a0f21a7fb2f531c7fefe0c3fd3ab
Tobias Krais <tk@design-to-use.de>
2022-02-10 18:21:23 +0100

    publish github to redaxo

--- pages/setup.php
+++ pages/setup.php
@@ -29,6 +29,7 @@ $d2u_module_manager->showManagerList();
 <h2>Changelog</h2>
 <p>1.1.4-DEV:</p>
 <ul>
+	<li>Anpassungen an Publish Github Release to Redaxo.</li>
 	<li>News können nun für einzelne Sprachen ausgeblendet werden.</li>
 	<li>Messen Plugin: Messen haben nun auch ein Feld für ein Bild.</li>
 	<li>Bugfix: Beim Löschen von Medien die vom Addon verlinkt werden wurde der Name der verlinkenden Quelle in der Warnmeldung nicht immer korrekt angegeben.</li>
