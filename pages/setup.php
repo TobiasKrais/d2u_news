2bdca96b50e075b6c21ebc6295be71ef8dd9da67
Tobias Krais <tk@design-to-use.de>
2022-03-14 13:40:42 +0100

    prepare next version

--- pages/setup.php
+++ pages/setup.php
@@ -27,6 +27,10 @@ $d2u_module_manager->showManagerList();
 <h2>Support</h2>
 <p>Fehlermeldungen bitte im <a href="https://github.com/TobiasKrais/d2u_news" target="_blank">GitHub Repository</a> melden.</p>
 <h2>Changelog</h2>
+<p>1.1.5-DEV:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>1.1.4:</p>
 <ul>
 	<li>Anpassungen an Publish Github Release to Redaxo.</li>
