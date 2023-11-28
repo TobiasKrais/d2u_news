97cc01cefaa672acf4a2281cf456cb9be952c428
Tobias Krais <tk@design-to-use.de>
2023-11-28 10:04:29 +0100

    prepare next version

--- pages/setup.php
+++ pages/setup.php
@@ -27,6 +27,10 @@ $d2u_module_manager->showManagerList();
 <h2>Support</h2>
 <p>Fehlermeldungen bitte im <a href="https://github.com/TobiasKrais/d2u_news" target="_blank">GitHub Repository</a> melden.</p>
 <h2>Changelog</h2>
+<p>1.1.6-DEV:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>1.1.5:</p>
 <ul>
 	<li>PHP-CS-Fixer Code Verbesserungen.</li>
